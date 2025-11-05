77eb4caa08311f851a437c97cae004c1a31e352a
shirmila2007 <shirmila2007@gmail.com>
2025-11-05 15:48:58 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { UserData } from './store.jsx'
-import Profile from './assets/profile.jsx'
-import Signin from './assets/signin.jsx'
+import Profile from './profile.jsx'
+import Signin from './signin.jsx'
 
 export default function App() {
   const name=UserData((state)=> state.name)
   return (
     <div>
-     { name ? <Profile/> :<Signin/>}
+     { name ? <Profile/> :<Signin />}
     </div>
   )
 }
